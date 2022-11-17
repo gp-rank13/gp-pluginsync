79f4f4e4bf6a8add09a25ab90d4d1c6028da46cb
gp-rank13 <gp.rank13@gmail.com>
2022-11-17 23:08:39 +1100

    Initial version

--- src/LibMain.h
+++ src/LibMain.h
@@ -2,80 +2,19 @@
 
 #include "gigperformer/sdk/GPMidiMessages.h"
 #include "gigperformer/sdk/GPUtils.h"
+#include "gigperformer/sdk/GigPerformerFunctions.h"
 #include "gigperformer/sdk/GigPerformerAPI.h"
 #include "gigperformer/sdk/types.h"
 
 class LibMain : public gigperformer::sdk::GigPerformerAPI
 {
-  protected:
-    int GetPanelCount() override;
-    std::string GetPanelName(int index) override;
-    std::string GetPanelXML(int index) override;
-
-    // These are for creating menu items in Gig Performer that can be used to
-    // trigger external functions provided by the extension developer
-    int GetMenuCount() override;
-    std::string GetMenuName(int index) override;
-    void InvokeMenu(int itemIndex) override;
-
   public:
-    // These must be here but no need to do anything unless you want extra behavior
-    explicit LibMain(LibraryHandle handle) : GigPerformerAPI(handle)
-    {
-    }
+    explicit LibMain(LibraryHandle handle) : GigPerformerAPI(handle) { }
+    ~LibMain() override { }
 
-    ~LibMain() override
-    {
-    }
-
-    void OnStatusChanged(GPStatusType status) override
-    {
-        consoleLog("Gig status changed to " + std::to_string(status));
-    }
-
-    void OnRackspaceActivated() override
-    {
-        consoleLog("Rackspace activated");
-    }
-
-    void OnSetlistChanged(const std::string &newSetlistName) override
-    {
-        consoleLog("Setlist switched to: " + newSetlistName);
-    }
-
-    void OnModeChanged(int mode) override;
-
-    void OnSwitchToPanelView() override;
-
-    void OnSwitchToWiringView() override;
-
-    void OnTempoChanged(double) override
-    {
-        consoleLog("Tempo changed");
-    }
-
-    void OnGlobalPlayStateChanged(bool playing) override;
-
-    // Now, simply override the callback methods in which you are interested
-    // and, in the Initialization method at the end of this class,
-    // call RegisterCallback for each of these methods
-
-    void OnWidgetValueChanged(const std::string &widgetName, double newValue) override;
-
-    // A midi device was added or removed
-    void OnMidiDeviceListChanged(std::vector<std::string> &inputs, std::vector<std::string> &outputs) override;
-
-    void OnWidgetCaptionChanged(const std::string &widgetName, const std::string &newCaption) override;
-
-    void OnSongChanged(int oldIndex, int newIndex) override;
-
-    void OnWidgetStateChanged(const std::string &widgetName, int newState) override;
-
+    void OnRackspaceActivated() override;
+    void OnVariationChanged(int oldIndex, int newIndex) override;
     void Initialization() override;
-
-    int RequestGPScriptFunctionSignatureList(GPScript_AllowedLocations location,
-                                             ExternalAPI_GPScriptFunctionDefinition **list) override;
-
-    // This MUST be defined in your class
+    void SyncPlugins();
     std::string GetProductDescription() override;
 };
